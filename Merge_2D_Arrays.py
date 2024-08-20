8eec106401a4e8db33a43eaa39388ec559081d0d
CamiloVelezP <39506281+CamiloVelezP@users.noreply.github.com>
2024-08-19 19:51:38 -0500

    Delete Merge_2D_Arrays.py

--- Merge_2D_Arrays.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def mergeArrays(self, nums1, nums2):
-        dict1 = {item[0]: item[1] for item in nums1 }
-        for item in nums2:
-            if item[0] in dict1:
-                dict1[item[0]] += item[1]
-            else:
-                dict1[item[0]] = item[1]
-        return sorted(dict1.items())
